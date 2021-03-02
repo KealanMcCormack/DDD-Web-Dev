d10c1a8fa9b318374baa79e2586c66b0d8ecab43
Gerard Colman <gerard.colman1@gmail.com>
2021-03-02 14:30:14 +0000

    Added dynamic product pages

--- src/main/java/app/MainController.java
+++ src/main/java/app/MainController.java
@@ -1,17 +1,22 @@
 package app;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 public class MainController {
 
-    ArrayList<Product> productList = new ArrayList<>();
+    Integer productID = 0;
+    HashMap<Integer, Product> productList = new HashMap<>();
     ArrayList<Customer> customerList = new ArrayList<>();
     ArrayList<Owner> ownerList = new ArrayList<>();
 
@@ -20,9 +25,12 @@ public class MainController {
 
     @GetMapping("/")
     public String gallery(Model model){
-        productList.add(new Product(1,12,"Kealan", "test"));
-        productList.add(new Product(2,0,"Lukas", "test1"));
-        productList.add(new Product(3, 1, "Gerard", "test3"));
+        productList.put(productID, new Product(productID,12,"Kealan", "test"));
+        productID++;
+        productList.put(productID, new Product(productID,2,"Lukas", "test1"));
+        productID++;
+        productList.put(productID, new Product(productID, 1, "Gerard", "test3"));
+        productID++;
 
         model.addAttribute("products", productList);
         return "gallery.html";
@@ -48,5 +56,16 @@ public class MainController {
         }
 
         return "login.html";
+    }
+
+    @GetMapping("/product/{id}")
+    public String productView(Model model, @PathVariable("id") int id){
+        Product selectedProduct = productList.get(id);
+        if(selectedProduct == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "product not found");
+        }
+        System.out.println(selectedProduct.toString());
+        model.addAttribute("product", selectedProduct);
+        return "product.html";
     }
 }
